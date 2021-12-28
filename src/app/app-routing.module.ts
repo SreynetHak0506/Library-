0480e3d782ad0d053511df2549fa89fda6c9df49
SreynetHak0506 <54494089+SreynetHak0506@users.noreply.github.com>
2021-12-28 13:16:30 +0700

    code library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {TestComponent} from '../app/test/test.component'
+import {AboutComponent} from '../app/about/about.component'
+import { ContactComponent } from './contact/contact.component';
+import { BooklistComponent } from './booklist/booklist.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'test-component', component: TestComponent },
+    { path: 'about-component', component: AboutComponent },
+    { path: 'contact-component', component: ContactComponent },
+    { path: 'book-list-component', component: BooklistComponent },
+
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { BooklistComponent } from './booklist/booklist.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    AboutComponent,
+    ContactComponent,
+    BooklistComponent,
   ],
   imports: [
     BrowserModule,
